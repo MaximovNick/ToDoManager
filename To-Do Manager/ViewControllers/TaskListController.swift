6e7101ac064b8394a1dee9b483305c2207216157
Nikolai Maximov <kolkamaximov@gmail.com>
2022-10-26 14:07:21 +0300

    working with application logic, displaying data in a table

--- To-Do Manager/ViewControllers/TaskListController.swift
+++ To-Do Manager/ViewControllers/TaskListController.swift
@@ -9,38 +9,126 @@ import UIKit
 
 class TaskListController: UIViewController {
     
+    // порядок отображения задач по их статусу
+    var tasksStatusPosition: [TaskStatus] = [.planed, .completed]
+    
+    // коллекция задач
+    var tasks: [TaskPriority: [TaskProtocol]] = [:]
+    
+    // порядок отображения секций по типам
+    // индекс в массиве соответствует индексу секции в таблице
+    var sectionsTypesPosition: [TaskPriority] = [.important, .normal]
+    
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.register(TaskCell.self, forCellReuseIdentifier: TaskCell.identifier)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.dataSource = self
-        
+        loadTasks()
+        title = "table view"
         view.addSubview(tableView)
+        setDelegates()
         setConstraints()
+        
+    }
+    
+    private func setDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
 
-// MARK: - Table View Data Source
-extension TaskListController: UITableViewDataSource {
+extension TaskListController {
+    
+    private func loadTasks() {
+        // подготовка коллекции с задачами
+        // будем использовать только те задачи, для которых определена секция в таблице
+        sectionsTypesPosition.forEach { taskType in
+            tasks[taskType] = []
+        }
+        
+        // загрузка и разбор задач из хранилища
+        TasksStorage.shared.loadTasks().forEach { task in
+            tasks[task.type]?.append(task)
+        }
+        
+        // сортировка списка задача
+        for (taskGroupPriority, tasksGroup) in tasks {
+            tasks[taskGroupPriority] = tasksGroup.sorted { task1, task2 in
+                let task1position = tasksStatusPosition.firstIndex(of: task1.status) ?? 0
+                let task2position = tasksStatusPosition.firstIndex(of: task2.status) ?? 0
+                return task1position < task2position
+            }
+        }
+    }
+}
+
+extension TaskListController: UITableViewDataSource, UITableViewDelegate {
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        tasks.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        // определяем приоритет задача, соответствующий текущей секции
+        let taskType = sectionsTypesPosition[section]
+        guard let currentTasksType = tasks[taskType] else { return 0 }
+        return currentTasksType.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: TaskCell.identifier, for: indexPath) as? TaskCell else { return UITableViewCell() }
-        cell.titleLabel.text = "123"
+        // загружаем прототип ячейки по идентификатору
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: TaskCell.identifier, for: indexPath) as? TaskCell else {
+            return UITableViewCell()
+        }
+        // получаем данные о задаче, которую необходимо вывести в ячейке
+        let taskType = sectionsTypesPosition[indexPath.section]
+        guard let currentTask = tasks[taskType]?[indexPath.row] else { return cell }
         
+        // изменим текст в ячейке
+        cell.titleLabel.text = currentTask.title
+        // изменим символ в ячейке
+        cell.symbolLabel.text = getSymbolForTask(with: currentTask.status)
+        
+        // изменяем цвет текста
+        if currentTask.status == .planed {
+            // изменяем цвет текста
+            cell.titleLabel.textColor = .black
+            cell.symbolLabel.textColor = .black
+        } else {
+            cell.titleLabel.textColor = .lightGray
+            cell.symbolLabel.textColor = .lightGray
+        }
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title: String?
+        let tasksType = sectionsTypesPosition[section]
+        if tasksType == .important {
+            title = "Важные"
+        } else if tasksType == .normal {
+            title = "Текущие"
+        }
+        return title
+    }
+    
+    // возвращаем символ для соответствующего типа задачи
+    private func getSymbolForTask(with status: TaskStatus) -> String {
+        var resultSymbol: String
+        if status == .planed {
+            resultSymbol = "\u{25CB}"
+        } else if status == .completed {
+            resultSymbol = "\u{25C9}"
+        } else {
+            resultSymbol = ""
+        }
+        return resultSymbol
     }
 }
 
