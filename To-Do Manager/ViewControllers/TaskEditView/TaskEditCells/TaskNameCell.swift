649abbf3a2966a8903ad4e9da62c51d0c124067e
Nikolai Maximov <kolkamaximov@gmail.com>
2022-10-27 19:35:37 +0300

    static cell with text field for task creation screen

---
+++ To-Do Manager/ViewControllers/TaskEditView/TaskEditCells/TaskNameCell.swift
@@ -0,0 +1,43 @@
+//
+//  TaskNameCell.swift
+//  To-Do Manager
+//
+//  Created by Nikolai Maksimov on 27.10.2022.
+//
+
+import UIKit
+
+class TaskNameCell: UITableViewCell {
+
+    static let identifier = "TaskNameCell"
+    
+    let textField: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "Введите задачу"
+        textField.font = UIFont.systemFont(ofSize: 17)
+        textField.borderStyle = .none
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(textField)
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor, constant: 0),
+            textField.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: 0),
+            textField.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor, constant: 0),
+            textField.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor, constant: 0)
+        ])
+    }
+}
