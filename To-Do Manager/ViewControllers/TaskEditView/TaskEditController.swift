4a611554bea6bfb98e4a171a220e5765e150de79
Nikolai Maximov <kolkamaximov@gmail.com>
2022-10-29 13:52:30 +0300

    saving a new task and transferring it to the main screen

--- To-Do Manager/ViewControllers/TaskEditView/TaskEditController.swift
+++ To-Do Manager/ViewControllers/TaskEditView/TaskEditController.swift
@@ -35,6 +35,21 @@ class TaskEditController: UITableViewController, TaskTypeControllerDelegate {
         tableView.register(TaskTypeCell.self, forCellReuseIdentifier: TaskTypeCell.identifier)
         tableView.register(TaskStatusCell.self, forCellReuseIdentifier: TaskStatusCell.identifier)
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Сохранить", style: .plain, target: self, action: #selector(saveTask))
+        
+    }
+    
+    @objc func saveTask() {
+        // временно
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cell = tableView.cellForRow(at: indexPath) as! TaskNameCell
+        let indexPath1 = IndexPath(row: 2, section: 0)
+        let cell1 = tableView.cellForRow(at: indexPath1) as! TaskStatusCell
+        let title = cell.textField.text ?? ""
+        let type = taskType
+        let status: TaskStatus = cell1.statusSwitch.isOn ? .completed : .planed
+        doAfterEdit?(title, type, status)
+        navigationController?.popViewController(animated: true)
     }
     
     // MARK: - Table view data source
@@ -49,10 +64,12 @@ class TaskEditController: UITableViewController, TaskTypeControllerDelegate {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         switch indexPath.row {
+            
         case 0:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: TaskNameCell.identifier) as? TaskNameCell else { return UITableViewCell() }
             cell.textField.text = taskText
             return cell
+            
         case 1:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: TaskTypeCell.identifier) as? TaskTypeCell else { return UITableViewCell() }
             cell.accessoryType = .disclosureIndicator
@@ -62,8 +79,8 @@ class TaskEditController: UITableViewController, TaskTypeControllerDelegate {
                 taskType = selectedType
                 cell.taskTypeLabel.text = taskTitles[taskType]
             }
-            
             return cell
+            
         default:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: TaskStatusCell.identifier) as? TaskStatusCell else { return UITableViewCell() }
             return cell
