ff95fffff299011b47a805b581b00732b4717479
Nikolai Maximov <kolkamaximov@gmail.com>
2022-10-30 17:55:58 +0300

    implementation of methods for saving, loading at startup, and deleting data from userDefaults

--- To-Do Manager/Services/TaskStorage.swift
+++ To-Do Manager/Services/TaskStorage.swift
@@ -9,30 +9,72 @@ import Foundation
 
 // протокол, описывающий сущность "Хранилище задач"
 protocol TasksStorageProtocol {
-    func loadTasks() -> [TaskProtocol]
+    func fetchTasks() -> [TaskProtocol]
     func saveTask(_ tasks: [TaskProtocol])
+    func deleteTask(at index: Int)
 }
 
 // Сущность "Хранилище задач"
-final class TasksStorage: TasksStorageProtocol {
+class TasksStorage: TasksStorageProtocol {
     
-    static let shared: TasksStorageProtocol = TasksStorage()
-    private init() {}
+    // Ссылка на хранилище
+    private var userDefaults = UserDefaults.standard
     
-    func loadTasks() -> [TaskProtocol] {
-        // временная реализация, возвращающая тестовую коллекцию задач
-        let testTasks: [TaskProtocol] = [
-            Task(title: "Купить хлеб", type: .normal, status: .planed),
-            Task(title: "Поммыть кота", type: .important, status: .planed),
-            Task(title: "Отдать долг Арнольду", type: .important, status: .completed),
-            Task(title: "Купить новый пылесос", type: .normal, status: .completed),
-            Task(title: "Подарить цветы супруге", type: .important, status: .planed),
-            Task(title: "Позвонить родителям", type: .important, status: .planed)
-        ]
-        return testTasks
+    // Ключ, по которому будет происходить сохранение и загрузка хранилища из User Defaults
+    var taskKey: String = "tasks"
+    
+    // Перечисление с ключами для записи в User Defaults
+    private enum TaskKey: String {
+        case title
+        case type
+        case status
+    }
+    
+    func fetchTasks() -> [TaskProtocol] {
+        var resultTasks: [TaskProtocol] = []
+        let tasksFromStorage = userDefaults.array(forKey: taskKey) as? [[String:String]] ?? []
+        for task in tasksFromStorage {
+            guard let title = task[TaskKey.title.rawValue],
+                  let typeRaw = task[TaskKey.type.rawValue],
+                  let statusRaw = task[TaskKey.status.rawValue] else {
+                continue
+                
+            }
+            let type: TaskPriority = typeRaw == "important" ? .important : .normal
+            let status: TaskStatus = statusRaw == "planned" ? .planned : .completed
+            resultTasks.append(Task(title: title, type: type, status: status))
+        }
+        return resultTasks
     }
     
     func saveTask(_ tasks: [TaskProtocol]) {
-        
+        var arrayForStorage: [[String:String]] = []
+        tasks.forEach { task in
+            var newElementForStorage: Dictionary<String, String> = [:]
+            newElementForStorage[TaskKey.title.rawValue] = task.title
+            newElementForStorage[TaskKey.type.rawValue] = (task.type == .important) ? "important" : "normal"
+            newElementForStorage[TaskKey.status.rawValue] = (task.status == .planned) ? "planned" : "completed"
+            arrayForStorage.append(newElementForStorage)
+        }
+        userDefaults.set(arrayForStorage, forKey: taskKey)
+    }
+    
+    
+    func deleteTask(at index: Int) {
+        let tasks = fetchTasks()
+        var arrayForStorage: [[String:String]] = []
+        tasks.forEach { task in
+            var newElementForStorage: Dictionary<String, String> = [:]
+            newElementForStorage[TaskKey.title.rawValue] = task.title
+            newElementForStorage[TaskKey.type.rawValue] = (task.type == .important) ? "important" : "normal"
+            newElementForStorage[TaskKey.status.rawValue] = (task.status == .planned) ? "planned" : "completed"
+            arrayForStorage.append(newElementForStorage)
+        }
+        arrayForStorage.remove(at: index)
+        userDefaults.set(arrayForStorage, forKey: taskKey)
     }
 }
+
+
+
+
